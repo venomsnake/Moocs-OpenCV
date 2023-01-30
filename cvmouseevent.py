504cd7e8185f8df2b5a3a5a1317f72a2c59773a9
venomsnake <thakurmark07@gmail.com>
2023-01-30 22:35:59 +0530

    OpenCv: MouseEvent BGR index

--- cvmouseevent.py
+++ cvmouseevent.py
@@ -15,11 +15,22 @@ def click_event(event, x, y, flags, param):
 		cv.putText(img, str_group, (x, y), fontf, 1, (255, 255, 0), 2)
 		cv.imshow('image', img)
 
+	if event == cv.EVENT_RBUTTONDOWN:
+		#print bgr channel
+		blue = img[y, x, 0]
+		green = img[y, x, 1]
+		red = img[y, x, 2]
+		fontf = cv.FONT_HERSHEY_SIMPLEX
+		str_group = str(blue) + ','+ str(green)+ ','+ str(red)
+		cv.putText(img, str_group, (x, y), fontf, 1, (255, 255, 0), 2)
+		cv.imshow('image', img)
 
-#image creation 
+'''#image creation 
 img = np.zeros((512, 512, 3), np.uint8)
 cv.imshow('image', img)
-
+'''
+img = cv.imread('test.png')
+cv.imshow('image', img)
 cv.setMouseCallback('image', click_event)
 
 cv.waitKey(0)
