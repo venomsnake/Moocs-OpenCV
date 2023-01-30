842e94f822a61022bc45a64c4fe4d8c1be03b694
venomsnake <thakurmark07@gmail.com>
2023-01-30 22:17:59 +0530

    OpenCv: Set Text on Video

---
+++ cvdraw.py
@@ -0,0 +1,31 @@
+#set property to capture and show current date time
+
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+print(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+#via set method 3/4 are number of the parameter
+
+cap.set(3, 1208)
+cap.set(4,720)
+print(cap.get(3))
+print(cap.get(4))
+while(cap.isOpened()):
+	ret, frame = cap.read() # read stream of video frame
+
+	if ret == True:
+		fontf = cv.FONT_HERSHEY_SIMPLEX
+		#frame will hold the data 
+		txt = 'Width: '+ str(cap.get(3)) + 'Height:' + str(cap.get(4))
+		frame = cv.putText(frame, txt, (10,50), fontf, 1, (0,255,255),2,cv.LINE_AA)
+		cv.imshow('frame', frame)
+
+		if cv.waitKey(1) == ord('q'):
+			break
+	else:
+		break
+
+cap.release()
+cv.destroyAllWindows()
