bd8a550c1efb7bac3306a006310bbdd8ad566491
venomsnake <thakurmark07@gmail.com>
2023-01-31 15:45:18 +0530

    OpenCV: Morphological Transformation [ADD]

--- cvmorpho.py
+++ cvmorpho.py
@@ -3,12 +3,25 @@ import numpy as np
 from matplotlib import pyplot as pp
 
 img = cv.imread("smarties.png", cv.IMREAD_GRAYSCALE)
+_, mask = cv.threshold(img, 220, 255, cv.THRESH_BINARY_INV)
 
-titles = ['image']
-images=[img]
+'''dilation transformation to hide black dot with white kernal'''
+kernal = np.ones((3,3), np.uint8) #square shape kernal
+dilation = cv.dilate(mask,kernal, iterations=2)
 
-for i in range(1):
-	pp.subplot(1, 1, i+1), pp.imshow(images[i], 'gray')
+'''erosion transformation
+'''
+erosion = cv.erode(mask, kernal, iterations=1)
+
+opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernal) #erosion then dialation
+
+closeing = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernal)
+
+titles = ['image', 'mask', 'dilation', 'erosion', 'opening']
+images=[img, mask, dilation, erosion, opening]
+
+for i in range(5):
+	pp.subplot(2, 3, i+1), pp.imshow(images[i], 'gray')
 	pp.title(titles[i])
 	pp.xticks([]) ,pp.yticks([])
 
