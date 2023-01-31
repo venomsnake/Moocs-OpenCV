90a39ccda2b65464b75f03765d44ba4867d579ca
venomsnake <thakurmark07@gmail.com>
2023-01-31 17:39:08 +0530

    OpenCV: Image Pyramid:Gaussian&Laplacian

---
+++ cvimagepyramids.py
@@ -0,0 +1,37 @@
+#Image Pyramids Gaussian and Laplasican 
+
+import cv2 as cv
+import numpy as np
+
+img = cv.imread("lena.jpg")
+'''
+	pyrDown and pyrUp methods
+
+lr = cv.pyrDown(img)
+lr1 = cv.pyrDown(lr)
+hr = cv.pyrUp(img)
+
+cv.imshow("Original Image", img)
+cv.imshow("pyrDown", lr)
+cv.imshow("Image", lr1)
+cv.imshow("PyrUp", hr)
+'''
+layer = img.copy() #copy image from source
+gp = [layer] #Gaussian pyramid 
+
+for i in range(6):
+	layer = cv.pyrDown(layer)
+	gp.append(layer)
+	#cv.imshow(str(i), layer)
+
+'''laplcian'''
+layer = gp[5]
+cv.imshow("upper level ",layer)
+lp = [layer]
+
+for i in range(5, 0, -1):
+	gaussian_extended = cv.pyrUp(gp[i])
+	laplacian = cv.subtract(gp[i-1], gaussian_extended)
+	cv.imshow(str(i), laplacian)
+cv.waitKey(0)
+cv.destroyAllWindows()
